eb92f4d670de0ef534bc128a7ee05159be9cd11b
HarshShah2807 <hhaarrsshh28@gmail.com>
2025-07-26 01:14:43 +0530

    Update leetcode34.py

--- leetcode34/leetcode34.py
+++ leetcode34/leetcode34.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findIndex(isFirst):
+            left, right = 0, len(nums) - 1
+            index = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    index = mid
+                    if isFirst:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return index
+        first = findIndex(True)
+        last = findIndex(False)
+        return [first, last]       
