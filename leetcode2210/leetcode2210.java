ee6c234c4bb02589dbfcdb31368a03138fd34e6f
HarshShah2807 <hhaarrsshh28@gmail.com>
2025-07-27 21:56:08 +0530

    Create leetcode2210.java

---
+++ leetcode2210/leetcode2210.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countHillValley(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] == nums[i - 1]) continue;
+            int prev = i - 1;
+            while (prev >= 0 && nums[prev] == nums[i]) {
+                prev--;
+            }
+            int next = i + 1;
+            while (next < n && nums[next] == nums[i]) {
+                next++;
+            }
+            if (prev >= 0 && next < n) {
+                if (nums[i] > nums[prev] && nums[i] > nums[next]) {
+                    count++; 
+                } else if (nums[i] < nums[prev] && nums[i] < nums[next]) {
+                    count++; 
+                }
+            }
+        }
+        return count;
+    }
+}
