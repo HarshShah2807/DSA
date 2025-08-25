b3f3f0fc63ee94694b87cf0b7ef6eb337c3c7a35
HarshShah2807 <hhaarrsshh28@gmail.com>
2025-08-25 14:08:47 +0530

    Update leetcode1679.py

--- leetcode1679/leetcode1679.py
+++ leetcode1679/leetcode1679.py
@@ -1,3 +1,4 @@
+#1
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         c=Counter(nums)
@@ -12,7 +13,7 @@ class Solution:
             
         return count
 
-
+#2
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         freq = Counter(nums)
@@ -27,4 +28,24 @@ class Solution:
                     count += pairs
                     freq[target] = 0  
                 freq[num] = 0  
+        return count
+
+#3
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left, right = 0, len(nums) - 1
+        count = 0
+        
+        while left < right:
+            s = nums[left] + nums[right]
+            if s == k:
+                count += 1
+                left += 1
+                right -= 1
+            elif s < k:
+                left += 1
+            else:
+                right -= 1
+                
         return count
