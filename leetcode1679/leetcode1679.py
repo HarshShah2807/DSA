307021d277b15922f0b52f95149810f6df5c98b5
HarshShah2807 <hhaarrsshh28@gmail.com>
2025-08-25 14:02:30 +0530

    Update leetcode1679.py

--- leetcode1679/leetcode1679.py
+++ leetcode1679/leetcode1679.py
@@ -11,3 +11,20 @@ class Solution:
                 c[i]-=1
             
         return count
+
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        freq = Counter(nums)
+        count = 0
+        for num in list(freq):
+            target = k - num
+            if target in freq:
+                if num == target:
+                    count += freq[num] // 2
+                else:
+                    pairs = min(freq[num], freq[target])
+                    count += pairs
+                    freq[target] = 0  
+                freq[num] = 0  
+        return count
